606b80edef2b74d212c9d3e2a1d5320a7bc5ed46
lvmor <lvm.lspa@gmail.com>
2019-03-18 15:58:42 -0700

    adds user and recipe models

---
+++ models.py
@@ -0,0 +1,31 @@
+import datetime
+
+from peewee import *
+
+DATABASE = SqliteDatabase('veggiedish.db')
+
+class User(Model):
+    full_name = CharField()
+    email = CharField()
+    password = CharField()
+    date_joined = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-datejoined',)
+
+class Recipe(Model):
+    name = CharField()
+    image = CharField()
+    description = TextField()
+    ingredients = TextField()
+    instructions = TextField()
+    average_rating = IntegerField(default=0)
+
+    class Meta:
+    database = DATABASE
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User, Recipe], safe=True)
+    DATABASE.close()
