c285e96d65c31420b0191f609f7e2a6192fedebf
lvmor <lvm.lspa@gmail.com>
2019-03-19 19:38:31 -0700

    updates models

--- models.py
+++ models.py
@@ -24,7 +24,7 @@ class Recipe(Model):
     average_rating = IntegerField(default=0)
 
     class Meta:
-    database = DATABASE
+        database = DATABASE
 
 class Review(Model):
     rating = IntegerField(default=0)
