b08193360af1b69212b6f117fcc838b714381ed4
Abimzy <eabiemo@gmail.com>
2019-03-22 03:13:47 -0700

    Add UserMixin to handle user session status

--- veggieproject/models.py
+++ veggieproject/models.py
@@ -1,13 +1,16 @@
 from datetime import datetime
-from veggieproject import db
+from veggieproject import db, login_manager
+from flask_login import UserMixin
 
 from peewee import *
 
-# DATABASE = SqliteDatabase('veggiedish.db')
+#This function is for reloading user from user_id stored in the session
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user.id))
 
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -15,7 +18,7 @@ class User(db.Model):
     password = db.Column(db.String(60), nullable=False)
     reviews = db.relationship('Review', backref='author', lazy=True) #This sets relationship between user and reviews
 
-#This shows how our user object looks when printed out
+#This shows how our user object looks when printed out(Excluding password!)
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
@@ -31,19 +34,6 @@ class Review(db.Model):
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
 
-
-
-
-# class User(Model):
-#     full_name = TextField()
-#     avatar = TextField()
-
-#     city = TextField()
-#     date_joined = DateTimeField(default=datetime.datetime.now)
-
-#     class Meta:
-#         database = DATABASE
-#         order_by = ('-date_joined',)
 
 # class Recipe(Model):
 #     name = CharField()
